494b7b0fc532e7e25afaed2d8790ec991ef25771
Asus <Bichk_di78@student.itstep.org>
2023-05-30 17:41:50 +0300

    3

--- main.py
+++ main.py
@@ -3,12 +3,8 @@ from bs4 import BeautifulSoup
 response = requests.get("https://uk.wikipedia.org/wiki/")
 if response.status_code == 200:
     req = BeautifulSoup(response.content, "html.parser")
-    table = req.find_all("table")
-    for i in table:
-        trs = i.find_all('tr')
-        for tr in trs:
-            finish = tr.find_all(['td', 'th'])
-            for text_in_row in finish:
-                print(text_in_row.get.text().strip)
+    classs = req.find_all("exampel_class")
+    for i in classs:
+        print(i)
 else:
-    print(f"Немає підкючення {response.status_code}")
+    print(f"Немає підключення {response.status_code}")
