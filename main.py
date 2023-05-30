4f6e55d605b921ad7c9152870160ffcf45f078e8
Asus <Bichk_di78@student.itstep.org>
2023-05-30 17:47:33 +0300

    4

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 import requests
 from bs4 import BeautifulSoup
-response = requests.get("https://uk.wikipedia.org/wiki/")
+response = requests.get("https://www.example.com/")
 if response.status_code == 200:
-    req = BeautifulSoup(response.content, "html.parser")
-    classs = req.find_all("exampel_class")
-    for i in classs:
-        print(i)
+    soup = BeautifulSoup(response.text,"html.parser")
+    for script in soup.find_all("style", "script"):
+        script.extract()
+    rezult = ' '.join(soup.stripped_strings)
+    req = len(rezult.split())
+    print(req)
 else:
-    print(f"Немає підключення {response.status_code}")
+    print(f"немає підклчення {response.status_code}")
