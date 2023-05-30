c2d57cf2408498f47a71ca9a2dfe6648b40f5d75
Asus <Bichk_di78@student.itstep.org>
2023-05-30 17:32:00 +0300

    1

--- main.py
+++ main.py
@@ -1,27 +1,13 @@
-import logging
-class StudentProcessor:
-    def __init__(self):
-        self.students = []
-        self.logger = logging.getLogger('hello')
-        self.logger.setLevel(logging.INFO)
-        self.logger.addHandler(logging.StreamHandler())
-    def add_student(self , student):
-        self.students.append(student)
-        self.logger.info(f"{student} був доданий до списку")
-    def remove_student(self , student):
-        try:
-          self.students.remove(student)
-          self.logger.info(f"{student} був видалений зі списку")
-        except:
-          print(f"{student} не було знайдено в списку")
-    def process_students(self):
-        print()
-        print("Студенти")
-        for item in self.students:
-            print(f"{item} студент")
-finish = StudentProcessor()
-finish.add_student("Захар")
-finish.add_student("Женя")
-finish.add_student("Даша")
-finish.remove_student("Даша")
-finish.process_students()
+import requests
+from bs4 import BeautifulSoup
+
+response = requests.get("https://uk.wikipedia.org/wiki/")
+if response.status_code == 200:
+    req = BeautifulSoup(response.content, "html.parser")
+    for i in req.find_all('h2'):
+        i.extract()
+    text = ' '.join(req.stripped_strings)
+    words = text.split()
+    print(words)
+else:
+    print(f"Немає підключення {response.status_code}")
